0b2fda0dbce5eade0338724545e1fc912269e47d
RekoLep <reko.leppanen@myy.haaga-helia.fi>
2025-03-17 17:28:46 +0200

    testi

---
+++ backend/yolento/src/test/java/com/backendprojekti/yolento/YolentoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.backendprojekti.yolento;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YolentoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
