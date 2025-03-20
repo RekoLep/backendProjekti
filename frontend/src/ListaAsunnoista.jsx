36a664801b128530f024f798fc8746fa373f4ac0
RekoLep <reko.leppanen@myy.haaga-helia.fi>
2025-03-20 15:15:30 +0200

    delete ominaisuus lisätty

--- frontend/src/ListaAsunnoista.jsx
+++ frontend/src/ListaAsunnoista.jsx
@@ -17,7 +17,7 @@ const ListaAsunnoista = () => {
       setEditAsunto({ ...asunto });
     }
   };
-// Hakee kaikki asunnot
+  // Hakee kaikki asunnot
   useEffect(() => {
     fetch("http://localhost:8080/api/asunnot")
       .then((response) => response.json())
@@ -40,76 +40,80 @@ const ListaAsunnoista = () => {
       headers: {
         "Content-Type": "application/json",
       },
-      body: JSON-stringify(uusiAsuntoData),
+      body: JSON - stringify(uusiAsuntoData),
     })
-    .then((response) => {
-      if (!response.ok) {
-        throw new Error("Virhe palvelimella: " + response.status);
-      }
-      return response.json();
-    })
-    .then((data) => {
-      setAsunnot([...asunnot, data]);
-      setUusiAsunto({
-        asiakasId: "",
-        asuntoId: "",
-        aloitusPvm: "",
-        loppuPvm: "",
-      });
-    })
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error("Virhe palvelimella: " + response.status);
+        }
+        return response.json();
+      })
+      .then((data) => {
+        setAsunnot([...asunnot, data]);
+        setUusiAsunto({
+          asiakasId: "",
+          asuntoId: "",
+          aloitusPvm: "",
+          loppuPvm: "",
+        });
+      })
   }
 
   // Päivittää tietoja
 
-  const handleUpdate = () =>{
-    fetch('http://localhost:8080/api/asunnot/${editAsunto.id}', {method: "PUT", headers: {"Content-Type": "application/json",},
-    body: JSON.stringify({
-      id: editAsunto.id,
-      osoite: editAsunto.osoite,
-      kaupunki: editAsunto.kaupunki,
-      kaupunginosa: editAsunto.kaupunginosa,
-      status: editAsunto.status,
-    })})
-    .then((response) => {
-      if (!response.ok) {
-        throw new Error("Päivitys epäonnistui.");
-      }
-      return response.json();
+  const handleUpdate = () => {
+    fetch('http://localhost:8080/api/asunnot/${editAsunto.id}', {
+      method: "PUT", headers: { "Content-Type": "application/json", },
+      body: JSON.stringify({
+        id: editAsunto.id,
+        osoite: editAsunto.osoite,
+        kaupunki: editAsunto.kaupunki,
+        kaupunginosa: editAsunto.kaupunginosa,
+        status: editAsunto.status,
+      })
     })
-    .then((data) => {
-      setAsunnot(
-        asunnot.map((v) =>
-          v.id === data.id ? data : v
-        )
-      );
-      setEditAsunto(null);
-    })
-    .catch((error) => console.error("Virhe vuokrasopimuksen päivittämisessä:", error));
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error("Päivitys epäonnistui.");
+        }
+        return response.json();
+      })
+      .then((data) => {
+        setAsunnot(
+          asunnot.map((v) =>
+            v.id === data.id ? data : v
+          )
+        );
+        setEditAsunto(null);
+      })
+      .catch((error) => console.error("Virhe vuokrasopimuksen päivittämisessä:", error));
   }
 
   // Poistaa asunnon
   const handleDelete = () => {
-    fetch('http://localhost:8080/api/asunnot/${deleteid}', {
+    fetch(`http://localhost:8080/api/asunnot/${deleteId}`, {
       method: "DELETE",
     })
-    .then((response) => {
-      if (!response.ok) {
-        throw new Error("Virhe palvelimella: " + response.status);
-      }
-      return response.text();
-    })
-    .then(() => {
-      setAsunnot(asunnot.filter((v) => v.id !== deleteId));
-      setDeleteId("");
-    })
-    .catch((error) =>
-      console.error("Virhe vuokrasopimuksen poistamisessa:", error)
-    );
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error("Virhe palvelimella: " + response.status);
+        }
+        return response.text();
+      })
+      .then(() => {
+        setAsunnot(asunnot.filter((v) => v.id !== deleteId));
+        setDeleteId("");
+      })
+      .catch((error) =>
+        console.error("Virhe vuokrasopimuksen poistamisessa:", error)
+      );
   }
 
   return (
     <div>
       <h1>Asunnot</h1>
+
+      {/*Asuntojen listaaminen*/}
       <table>
         <thead>
           <tr>
@@ -128,10 +132,26 @@ const ListaAsunnoista = () => {
               <td>{asunto.kaupunki}</td>
               <td>{asunto.kaupunginOsa}</td>
               <td>{asunto.status}</td>
+              <td>
+                <button onClick={() => setDeleteId(asunto.asuntoId)}>Poista</button>
+              </td>
             </tr>
           ))}
         </tbody>
       </table>
+
+      {/* Asuntojen poistaminen poistaminen */}
+      {deleteId && (
+        <div>
+          <p>Oletko varma, että haluat poistaa asunnon ID:llä {deleteId}?</p>
+          <button onClick={handleDelete}>Kyllä</button>
+          <button onClick={() => setDeleteId("")}>Peruuta</button>
+        </div>
+      )}
+
+
+
+
       <Link to="/index">Takaisin etusivulle</Link>
     </div>
   );
